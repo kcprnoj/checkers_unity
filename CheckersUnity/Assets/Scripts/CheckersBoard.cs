2579a3b34270b4e8767c718c14e5b3259f9ae09c
kcprnoj <54915392+kcprnoj@users.noreply.github.com>
2020-12-29 13:29:54 +0100

    Change Board and asset names

---
+++ CheckersUnity/Assets/Scripts/CheckersBoard.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckersBoard : MonoBehaviour
+{
+    private Piece selectedPawn;
+    public int BlackLeft;
+    public int WhiteLeft;
+    public int BlackKings;
+    public int WhiteKings;
+    public Piece[,] Board;
+
+    public CheckersBoard()
+    {
+        BlackLeft = 12;
+        WhiteLeft = 12;
+        BlackKings = 0;
+        WhiteKings = 0;
+        Board = new Piece[8, 8];
+        CreateBoard();
+    }
+
+    public void CreateBoard()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if ((i + j) % 2 == 1)
+                {
+                    if (i < 3)
+                        Board[i, j] = new Piece(i, j, 1);
+                    else if (i > 4)
+                        Board[i, j] = new Piece(i, j, 2);
+                    else
+                        Board[i, j] = new Piece(i, j, 0);
+                }
+                else
+                    Board[i, j] = new Piece(i, j, 0);
+            }
+        }
+    }
+
+    public void Draw()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Board[i, j].Draw();
+            }
+            Console.Write("\n");
+        }
+    }
+
+    public Piece GetPiece(int row, int col)
+    {
+        return Board[row, col];
+    }
+
+    public void Move(Piece piece, int row, int col)
+    {
+        Piece temp = Board[piece.Row, piece.Col];
+        Board[piece.Row, piece.Col] = Board[row, col];
+        Board[row, col] = temp;
+
+        piece.Move(row, col);
+
+        if (row == 8 || row == 0)
+        {
+            piece.King = true;
+            if (piece.Color == 1)
+                BlackKings++;
+            else
+                WhiteKings++;
+        }
+    }
+}
---
+++ CheckersUnity/Assets/Scripts/Piece.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour
+{
+    public class Piece
+    {
+        public int Row;
+        public int Col;
+        public int Color;
+        public bool King;
+        public int Direction;
+
+        public Piece(int row, int col, int color)
+        {
+            Row = row;
+            Col = col;
+            Color = color;
+            King = false;
+            if (color == 1)
+                Direction = 1;
+            else
+                Direction = -1;
+        }
+
+        public void Draw()
+        {
+            
+        }
+
+        public void Move(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+    }
+}
