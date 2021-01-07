987adb12f481a73c10e81885491effc5b5f5868b
milenadob <56935119+milenadob@users.noreply.github.com>
2021-01-08 00:01:34 +0100

    fixed my 2iq ranking display

--- CheckersUnity/Assets/Scripts/DatabaseScripts/RankingElementDisplay.cs
+++ CheckersUnity/Assets/Scripts/DatabaseScripts/RankingElementDisplay.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class RankingElementDisplay : MonoBehaviour
+public class RankingElementDisplay : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public GameObject rank;
     public GameObject playerName;
     public GameObject gamesWonNumber;
     public GameObject gamesLostNumber;
     public GameObject bestGame;
+    public GameObject rankDetails;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rankDetails.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        rankDetails.SetActive(false);
+    }
 
     public void SetRankingElement(RankingElement elem)
     {
@@ -17,6 +29,18 @@ public class RankingElementDisplay : MonoBehaviour
         this.playerName.GetComponent<TMPro.TextMeshProUGUI>().text = elem.Name;
         this.gamesWonNumber.GetComponent<TMPro.TextMeshProUGUI>().text = elem.NumberOfWins.ToString();
         this.gamesLostNumber.GetComponent<TMPro.TextMeshProUGUI>().text = elem.NumberOfDefeats.ToString();
-        this.bestGame.GetComponent<TMPro.TextMeshProUGUI>().text = "Best Game: Side -> " + elem.Side + "Time -> " + elem.Time.ToString() + " s";
+        this.bestGame.GetComponent<TMPro.TextMeshProUGUI>().text = "Best Game:\nSide -> " + elem.Side + "\tTime -> " + elem.Time.ToString() + " s";
     }
+
+    public void ToggleDetails()
+    {
+        if (rankDetails.activeSelf)
+            rankDetails.SetActive(false);
+        else
+        {
+            rankDetails.GetComponent<RectTransform>().SetAsLastSibling();
+            rankDetails.SetActive(true); 
+        }
+    }
+
 }
